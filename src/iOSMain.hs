8456cdbfb782ee99c75092079a4ee928bc43c71d
Erik Deijl <erik.deijl@gmail.com>
2015-12-04 16:12:11 +0100

    rect follows cursor

--- src/iOSMain.hs
+++ src/iOSMain.hs
@@ -5,6 +5,7 @@ module Main where
 import           Control.Concurrent
 import           Control.Monad
 import           Events
+import           Foreign.C.Types
 import           Graphics.UI.SDL            as SDL
 import           Reactive.Banana
 import           Reactive.Banana.Frameworks
@@ -17,7 +18,7 @@ screenHeight = 480
 foreign export ccall "haskell_main" main :: IO ()
 main = do
   SDL.init [SDL.InitVideo]
-  window <- SDL.createWindow "HXNetworking" (SDL.Position 0 0) (SDL.Size screenWidth screenHeight) []
+  window <- SDL.createWindow "HXNetworking" (SDL.Position 0 0) (SDL.Size screenWidth screenHeight) [WindowFullscreenDesktop]
   renderer <- SDL.createRenderer window (SDL.Device (-1)) []
   g <- newStdGen
   (frameAddHandler, fireFrame) <- newAddHandler
@@ -30,9 +31,12 @@ main = do
            e <- SDL.waitEvent
            case fmap SDL.eventData e of
              Just Quit -> SDL.quit
-             _ -> fireEvent e >> loop
+             Just ev -> print ev >> fireEvent ev >> loop
+             _ -> loop
   loop
 
+rectAtPosition :: (Int, Int) -> Rect -> Rect
+rectAtPosition (x, y) rect = Rect x  y (rectW rect) (rectH rect)
 randomRect :: StdGen -> Rect
 randomRect gen =
   Rect x y w h
@@ -47,23 +51,32 @@ initRect = Rect 100 100 100 100
 render :: Window -> Renderer -> Rect -> IO ()
 render window renderer rect = do
   print window
+  print rect
   setRenderDrawColor renderer 0 0 0 255
   renderClear renderer
-  r <- randomRIO (50, 255)
-  g <- randomRIO (50, 255)
-  b <- randomRIO (50, 255)
-  setRenderDrawColor renderer r g b 255
+  setRenderDrawColor renderer 255 0 0 255
   renderFillRect renderer rect
   renderPresent renderer
 
-makeNetwork :: forall t. Frameworks t => SDL.Window -> SDL.Renderer -> StdGen -> AddHandler () -> AddHandler (Maybe SDL.Event) -> Moment t ()
+makeNetwork :: forall t. Frameworks t => SDL.Window -> SDL.Renderer -> StdGen -> AddHandler () -> AddHandler EventData -> Moment t ()
 makeNetwork window renderer g frameAddHandler eventAddHandler = do
   frames <- fromAddHandler frameAddHandler
   events <- fromAddHandler eventAddHandler
   let bRect :: Behavior t Rect
-      bRect = accumB initRect (f <$> events)
-        where
-          f :: (Maybe SDL.Event) -> Rect -> Rect
-          f _ _ = randomRect g
+      bRect = accumB initRect (handleSDLEvent <$> events)
   eRect <- changes bRect
   reactimate' $ fmap (render window renderer ) <$> eRect
+
+handleSDLEvent :: EventData -> Rect -> Rect
+handleSDLEvent event rect = case event of
+                              MouseMotion _ _ _ pos _ _ -> rectAtPosition (positionX pos, positionY pos) rect
+                              --TouchFinger _ _ _ _ x y _ _ _ -> rectAtPosition (round (x * 1000 / 65535 * fromIntegral screenWidth) , round (y * 1000 / 65535 * fromIntegral screenHeight) ) rect
+                              _ -> rect
+
+
+
+inputCoordsToScreenCoords :: (CFloat, CFloat) -> (Int, Int)
+inputCoordsToScreenCoords (x, y) = (round (fromIntegral screenWidth * x), round (fromIntegral screenHeight * y))
+
+screenCoordsToInputCoords :: (Int, Int) -> (Float, Float)
+screenCoordsToInputCoords (x, y) = (fromIntegral x/ fromIntegral screenWidth , fromIntegral y/ fromIntegral screenWidth)
